847a920b64ec6693a70668607c5745820ce08f64
Anson Yu Wang <opensource@undozen.com>
2014-12-17 12:05:36 +0800

    ok, done

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 'use strict';
+
+module.exports = rewrite;
+
+function escapeRegExp(string) {
+    return string.replace(/([.*+?^${}()|\[\]\/\\])/g, "\\$1");
+}
+
+function rewrite(opts, contents) {
+    var revMap = opts.revMap || {};
+    var assetPathPrefix = opts.assetPathPrefix || '/assets/';
+    var revPost = typeof opts.revPost === 'function' ?
+        opts.revPost : function (p) {
+            return p;
+        };
+    /*
+     * copy from broccoli-assets-rewrite with some modification
+     *
+     * Replace all of the assets with their new fingerprint name
+     *
+     * Uses a regular expression to find assets in html tags, css backgrounds, handlebars pre-compiled templates, etc.
+     * (
+     * ["\'\\(=]{1} - Match one of "'(= exactly one time
+     * \\s* - Any amount of white space
+     * ) - here ends $1 (left)
+     * escapeRegExp(assetPathPrefix) - prefix is costomizable，default is '/assets/'
+     * ( - $2 (filePath) begins
+     * [^"\'\\(\\)\\\\>]* - Do not match any of ^"'()\> 0 or more times - Explicitly add \ here because of handlebars compilation
+     * )( - $3 (right) begins
+     * \\s* - Any amount of white space
+     * [\\\\]* - Allow any amount of \ - For handlebars compilation (includes \\\)
+     * \\s* - Any amount of white space
+     * ["\'\\)> ]{1} - Match one of "'( > exactly one time
+     */
+
+    var filePathRegex = new RegExp('(["\'\\(=]{1}\\s*)' + escapeRegExp(
+            assetPathPrefix) +
+        '([^"\'\\(\\)\\\\>]*)(\\s*[\\\\]*\\s*["\'\\)> ]{1})', 'g');
+    return contents.replace(filePathRegex, function (all, left,
+        filePath, right) {
+        var index, indexs, indexh, search;
+        //filePath = filePath.substring('/assets/'.length);
+        if ((indexs = filePath.indexOf('?')) > -1 || (indexh = filePath.indexOf(
+            '#')) > -1) {
+            //like ../../font/fontawesome-webfont.svg#fontawesomeregular?v=3.2.1
+            index = 'undefined' !== typeof indexh ? indexh :
+                (indexh = filePath.indexOf('#')) > -1 ? Math.min(indexs,
+                indexh) : indexs;
+            search = filePath.substring(index);
+            filePath = filePath.substring(0, index);
+        }
+        if (revMap[filePath]) {
+            return left + revPost(revMap[filePath]) + (search || '') +
+                right;
+        }
+        return all;
+    });
+}
---
+++ test/rewrite.js
@@ -0,0 +1,48 @@
+'use strict';
+var tape = require('tape');
+var rewrite = require('../');
+
+tape('keep original', function (t) {
+    t.plan(1);
+    var original = '<script src="/assets/a.js"></script>';
+    var result = rewrite({}, original);
+    t.equal(result, original);
+});
+
+tape('replace with revMap without leading slash', function (t) {
+    t.plan(1);
+    var original = '<script src="/assets/a.js"></script>';
+    var result = rewrite({
+        revMap: {
+            'a.js': 'b.js'
+        }
+    }, original);
+    t.equal(result, '<script src="b.js"></script>');
+});
+
+tape('replace with revMap and assetPathPrefix and leading slash', function (t) {
+    t.plan(1);
+    var original = '<script src="/static/a.js"></script>';
+    var result = rewrite({
+        assetPathPrefix: '/static',
+        revMap: {
+            '/a.js': '/b.js'
+        }
+    }, original);
+    t.equal(result, '<script src="/b.js"></script>');
+});
+
+tape('well all options setted', function (t) {
+    t.plan(1);
+    var original = '<script src="/static/a.js"></script>';
+    var result = rewrite({
+        assetPathPrefix: '/static',
+        revMap: {
+            '/a.js': '/b.js'
+        },
+        revPost: function(revvedFilePath) {
+            return '/assets' + revvedFilePath;
+        }
+    }, original);
+    t.equal(result, '<script src="/assets/b.js"></script>');
+});
